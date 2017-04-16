b110fa37e6894281d31a75783dfbd0a7d6415465
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-04-16 18:49:11 -0400

    group structure of jacobian

--- 2.py
+++ 2.py
@@ -1,10 +1,6 @@
-# folk examples from http://doc.sagemath.org/html/en/reference/curves/sage/schemes/hyperelliptic_curves/jacobian_generic.html
-
+# lookslike 2x10x20
 q = 13
-TH = 13
-listorder = []
-for i in range (TH):
-    listorder.append(0)
+TH = 52
 
 FF = FiniteField(q)
 R.<x> = PolynomialRing(FF)
@@ -14,37 +10,79 @@ C = HyperellipticCurve(f)
 n = C.count_points(1)
 print "number of points:", n, "factor n:", factor(n[0])
 
-#rpt = C.rational_points()
-#print rpt
 J = C.jacobian()
 X = J(FF)
-print "X:",X
+print "X:", X   # fake set
 print "Frobenius:",C.frobenius_polynomial()
 
-counterJ=0  #this is a fake counter
-# try to find points on jacobian by enumeration
-for i in range(q):
-    for j in range(q):
-        a = x**2 + i*x + j
-        for k in range(q):
-            for l in range(q):
-                b = k*x + l  # TBD: compute square root
-                try:
-                    D = X([a,b])
-                    counterJ=counterJ+1
-                    cz = 2
-                    while (cz<TH):
-                        czD = cz*D
-                        if czD==D:
-                            print "ijkl:",i,j,k,l
-                            print cz-1, D  # (cz-1)*D == (1)
-                            listorder[cz-1]+=1
-                            break
-                        else:
-                            cz=cz+1
-#                    if cz == TH:
- #                       print "order>",TH
-                except ValueError:
-                    continue
-print counterJ, factor(counterJ)
-print listorder
+
+def quicker_enumeratepoints():
+    points = [] # list of all the points
+    listorder = []  # the group structure
+    for i in range (TH):
+        listorder.append(0)
+
+    for i in range(q):
+        for j in range(q):
+            a = x**2 + i*x + j
+            for k in range(q):
+                for l in range(q):
+                    b = k*x + l  # TBD: compute square root
+                    try:
+                        D = X([a,b])
+                        if D not in points:
+                            points.append(D)
+                        cz = 2
+                        while (cz<TH):
+                            czD = cz*D
+                            if czD==D:
+                                print "ijkl:",i,j,k,l
+                                print cz-1, D  # (cz-1)*D == (1)
+                                listorder[cz-1]+=1
+                                break
+                            else:
+                                cz=cz+1
+                                if czD not in points:
+                                    points.append(czD)
+                            if cz == TH:
+                                print "order>",TH
+                    except ValueError:
+                        continue
+    print listorder
+    print points, len(points)
+
+
+def bruteforce_enumeratepoints():
+    counterJ=0  #this is a fake counter
+    # try to find points on jacobian by enumeration
+    listorder = []
+    for i in range (TH):
+        listorder.append(0)
+
+    for i in range(q):
+        for j in range(q):
+            a = x**2 + i*x + j
+            for k in range(q):
+                for l in range(q):
+                    b = k*x + l  # TBD: compute square root
+                    try:
+                        D = X([a,b])
+                        counterJ=counterJ+1
+                        cz = 2
+                        while (cz<TH):
+                            czD = cz*D
+                            if czD==D:
+                                print "ijkl:",i,j,k,l
+                                print cz-1, D  # (cz-1)*D == (1)
+                                listorder[cz-1]+=1
+                                break
+                            else:
+                                cz=cz+1
+    #                    if cz == TH:
+     #                       print "order>",TH
+                    except ValueError:
+                        continue
+    print counterJ, factor(counterJ)
+    print listorder
+
+quicker_enumeratepoints()
