cbafe8069ae96170c2314096c0274ab575c57252
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-04-21 17:48:04 -0400

    Textbook miller by cantor's algorithm

--- 1.py
+++ 1.py
@@ -1,97 +1,113 @@
-# Weil Pairing Example
+# Weil Pairing Example, paired by Cantor's algorithm
 # Example 8.3 from Silverman
-# manually forked from https://github.com/guanzhi/CryptoWithSageMath
-
 # E: y^2 = x^3 + 30x + 34 mod 631
-#E(K) = 5x130
+# E(K) = 5x130
 
-p = 631
-K = GF(p)
-a = 30
-b = 34
-E = EllipticCurve(K, [a, b])
-print E.count_points(1)
+TH = 10000  # threshold order
 
-def find_points_with_given_order(x,ord):
-    y2 = K(x^3 + a*x + b)
-    yr = K(y2).nth_root(2)
-    return yr
+q = 631
+FF = FiniteField(q)
+R.<x> = PolynomialRing(FF)
+f = x**3 + 30*x + 34
+C = HyperellipticCurve(f)
+n = C.count_points(1)
+genus = C.genus()
+print "genus:", genus, "number of points:", n, "factor n:", factor(n[0])
 
-#print find_points_with_given_order(5,5)
-    
-Z = E((5, 373))
-P = E((36, 60))
-Q = E((121, 387))
-n = 5
-S = E((0, 36))
+J = C.jacobian()
+X = J(FF)
+print "X:", X   # fake set
+print "Frobenius:", C.frobenius_polynomial()
 
-print "Z =", Z.xy()
-print "P =", P.xy()
-print "Q =", Q.xy()
-print "order of P = ", P.order(), "; order of Z = ", Z.order()
+# P = E((36, 60))
+# Q = E((121, 387))
+# n = 5
 
-for i in range(1,10):
-        print (P*i), (Q*i), (Z*i)
+a1 = x - 36
+b1 = 60
+a2 = x - 121
+b2 = 387
 
-var('x y')
+D1 = X([a1,b1])
+D2 = X([a2,b2])
+ide = X([1,0])
+print "D1 = ", D1, "D2 = ", D2
 
-def g(P, Q):
-	(x_P, y_P) = P.xy()
-	(x_Q, y_Q) = Q.xy()
-	if x_P == x_Q and y_P + y_Q == 0:
-		return x - x_P
-	if P == Q:
-		slope = (3 * x_P^2 + a)/(2 * y_P)
-	else:
-		slope = (y_P - y_Q)/(x_P - x_Q)
-	return (y - y_P - slope * (x - x_P))/(x + x_P + x_Q - slope^2)
+def MillerH(u1, v1, u2, v2, uE, vE):
+    """ Input: D1 = [u1, v1], D2 = [u2, v2], E = [uE, vE].
+        Output: Reduced divisor ρ(D1 + D2) and evaluation h^norm_{D1,D2}(E), represented by [h˜1(x), h˜2(x),h3].
+        H=0
+    """
+    d1, e1, e2 = xgcd(u1,u2)
+    d, c1, c2 = xgcd(d1,v1+v2)
+    h1 = d % uE
+    print "h1, uE, d:", h1, uE, d
+    h2 = 1
+    h3 = 1
+    s1 = c1*e1
+    s2 = c1*e2
+    s3 = c2
+    dinv = d**(-1)
+    u = R(u1*u2*dinv*dinv)
+    #print "u,d", type(u), type(u1), type(d)
+    v = R((s1*u1*v2 + s2*u2*v1 + s3*(v1*v2+f ))*dinv)%u
+    print "u:",u,"v:",v
+    while (u.degree()>genus):
+        ut = R((f - v*v)/u)
+        print "ut:", ut
+        ut = ut/ut.lc()
+        print "ut:", ut
+        vt = (-v) % ut
+        h1 = (h1*(vE - v)) % uE
+        h2 = (h2*ut) % uE
+        if v.degree()>genus:
+            h3 = -(v.lc())*h3
+        u = ut
+        v = vt
+    return u, v, h1, h2, h3
 
-def miller(m, P):
-	m = bin(m)[3:]
-	n = len(m)
-	T = P
-	f = 1
-	for i in range(n):
-		f = f^2 * g(T, T)
-		T = T + T
-		if int(m[i]) == 1:
-			f = f * g(T, P)
-			T = T + P
-	return f
+#MillerH(a1, b1, a2, b2, a1, b1)
 
-def muller(m, P):
-    # compute f_m,P by bruteforce, but ...
-	(x1, y1) = P.xy()
-        f=((x-x1)**m) *((-x/y)**m)  # this is the wrong formula, in a downtoearth term, div(x-x1) = (P)+(-P)-2(infty)
-	return f
+def FD(m, u1, v1, u2, v2):
+    """ miller's algorithm for hyperelliptic curve pairing
+    Input: m, D1 = [u1, v1], D2 = [u2, v2].
+    Output: Pairing value f_{m,D1}(eff(D2))
+    """
+    mbits = bin(m)[3:]  # bin(m)=0b...
+    n = len(mbits)
+    u = u1
+    v = v1
+    f = 1
+    f1 = 1
+    f2 = 1
+    f3 = 1
+    for i in range(n):
+        f1 = (f1*f1) % u2
+        f2 = (f2*f2) % u2
+        f3 = f3*f3
+        u, v, h1, h2, h3 = MillerH(u, v, u, v, u2, v2)
+        f1 = (f1*h1) % u2
+        f2 = (f2*h2) % u2
+        f3 = f3*h3
+        if int(mbits[i])==1:
+            u, v, h1, h2, h3 = MillerH(u, v, u1, v1, u2, v2)
+            f1 = (f1*h1) % u2
+            f2 = (f2*h2) % u2
+            f3 = f3*h3
+    f = u2.resultant(f1)/( (f3**(u2.degree()))*u2.resultant(f2) )
+    return f
 
-def eval_miller(P, Q):
-	f = miller(n, P)
-        print "f:", f, "P:", P
-	(x1, y1) = Q.xy()
-	return f(x = x1, y = y1)
+print FD(5, a1, b1, a2, b2)
+print FD(5, a2, b2, a1, b1)
 
-def weil_pairingS(P, Q, S):
-	num = eval_miller(P, Q+S)/eval_miller(P,  S)
-	den = eval_miller(Q, P-S)/eval_miller(Q, -S)
-	return (num/den)
 
-def weil_pairing(P, Q):
-	num = eval_miller(P, Q)
-	den = eval_miller(Q, P)
-	return ((-1)**n)*(num/den)
 
-e = weil_pairing(P, Q)
-print "e(P, Q) =", e
-
-# e^n = 1
-print "e(P, Q)^n =", e^n
-
-P3 = P * 3
-Q4 = Q * 4
-e12 = weil_pairing(P3, Q4)
-
-print "[3]P =", P3.xy()
-print "[4]Q =", Q4.xy()
-print "e([3]P, [4]Q) =", e12
-print "e(P, Q)^12 =", e^12
+def Jacobian_order(D):
+    """ input a divisor, output its order """
+    ide = X([1,0])
+    i=2
+    while (i<TH):
+        if i*D == ide:
+            return i
+        else:
+            i=i+1
