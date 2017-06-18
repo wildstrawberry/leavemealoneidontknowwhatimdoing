dda65d4df241847992342cecc32ba61a44de7211
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-06-18 18:59:49 -0400

    curves and jacobians

---
+++ 7.py
@@ -0,0 +1,6 @@
+# 2 or 3 curves I know about their jacobians
+# q = 71
+# f = x^5 + 12
+# C = HyperellipticCurve(f)
+# genus: 2 number of points: [53] factor n: 53
+# Frobenius: x^4 - 19*x^3 + 201*x^2 - 1349*x + 5041  #J(C) =  3875 5^3 * 31
