4701f8ce2f4036c048c8ec46962f8e7019df7e0d
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-06-20 22:36:22 -0400

    add a curve

--- 7.py
+++ 7.py
@@ -4,3 +4,7 @@
 # C = HyperellipticCurve(f)
 # genus: 2 number of points: [53] factor n: 53
 # Frobenius: x^4 - 19*x^3 + 201*x^2 - 1349*x + 5041  #J(C) =  3875 5^3 * 31
+
+# q = 113
+# hy = x^5 + x^2 + 53*x + 11
+# Frobenius: x^4 - 32*x^3 + 482*x^2 - 3616*x + 12769  #J(C) =  9604 2^2 * 7^4
