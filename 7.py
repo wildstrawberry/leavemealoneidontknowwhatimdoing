8bef054276177ec5b74373c54d8f45f51b857428
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-06-21 22:59:39 -0400

    add torsion points

--- 7.py
+++ 7.py
@@ -4,7 +4,12 @@
 # C = HyperellipticCurve(f)
 # genus: 2 number of points: [53] factor n: 53
 # Frobenius: x^4 - 19*x^3 + 201*x^2 - 1349*x + 5041  #J(C) =  3875 5^3 * 31
+# 5-torsions: 
 
 # q = 113
 # hy = x^5 + x^2 + 53*x + 11
 # Frobenius: x^4 - 32*x^3 + 482*x^2 - 3616*x + 12769  #J(C) =  9604 2^2 * 7^4
+# 7-torsions: 
+# P = X([x^2 + 4*x + 63, 10*x + 26])
+# Q = X([x^2 + 4*x + 24, 38*x + 65])
+# R = X([x^2 + 58*x + 94, 68*x + 56])
