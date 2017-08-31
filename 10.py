5895ad4333667467e71d2103645df11222f5478a
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-08-31 08:14:01 -0400

    Create 10.py

---
+++ 10.py
@@ -0,0 +1,27 @@
+# from http://doc.sagemath.org/html/en/reference/curves/sage/schemes/elliptic_curves/ell_curve_isogeny.html
+
+E = EllipticCurve(GF(37), [0,0,0,1,8])
+E.points()
+E.count_points(1)
+R.<x> = GF(37)[]
+f = x^3 + x^2 + 28*x + 33  # This is the The subscheme can either be given by a list of generators, which have to be torsion points, or by a polynomial in the coordinate x of the Weierstrass equation of E1
+phi = EllipticCurveIsogeny(E, f)
+phi_hat = phi.dual()
+print phi, phi.rational_maps()
+print phi_hat, phi_hat.rational_maps()
+
+P = E(3,1)
+P.order()
+
+Q = phi_hat(phi(P))
+phi2 = EllipticCurveIsogeny(E, Q)
+phi2_hat = phi2.dual()
+print phi2_hat
+
+print phi2_hat(phi2(Q))
+
+phi7 = E.multiplication_by_m(7)
+phi5 = E.multiplication_by_m(5)
+
+print "multiply-by-7 map", phi7
+print "multiply-by-5 map", phi5
