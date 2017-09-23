39cdf73b770d92726811c6201b80c092ecfc89e5
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-09-23 17:25:30 -0400

    add another example

--- 10.py
+++ 10.py
@@ -2,38 +2,37 @@
 
 E = EllipticCurve(GF(37), [0,0,0,1,8])
 print "j-inv of E: ", E.j_invariant()
-E.points()
-E.count_points(1)
+print "all the points on E: ",E.points()
+print "Number of points on E: ",E.count_points(1)
 R.<x> = GF(37)[]
+
 f = x^3 + x^2 + 28*x + 33  # kernel polynomial
-phi = EllipticCurveIsogeny(E, f)
+f.roots()
+phi = EllipticCurveIsogeny(E, f)  # generate an isogeny from kernel polynomial
 phi_hat = phi.dual()
-print phi, phi.rational_maps()
-print phi_hat, phi_hat.rational_maps()
+print phi #, phi.rational_maps()
+print phi_hat #, phi_hat.rational_maps()
 (X, Y) = phi.rational_maps()
 (Xhat, Yhat) = phi_hat.rational_maps()
 
-P = E(3,1)
-P.order()
+P = E(25,9)
+print "P and its order: ", P, P.order(), 2*P, 3*P
 
-Q = phi_hat(phi(P))
-phi2 = EllipticCurveIsogeny(E, Q)
-phi2_hat = phi2.dual()
-print phi2_hat
+phi2 = EllipticCurveIsogeny(E, P)
 
-print phi2_hat(phi2(Q))
+phi3 = E.multiplication_by_m(3)
+print phi3
+#phi5 = E.multiplication_by_m(5)
 
 
-#Xm = Xhat.subs(x=X, y=Y)
-#Ym = Yhat.subs(x=X, y=Y)
-#print "Xm", Xm
-phi7 = E.multiplication_by_m(7)
-phi5 = E.multiplication_by_m(5)
+E2 = EllipticCurve(GF(1009), [0,0,0,1,3])
+factor(1060)
+print E2
+print "Number of points on E2: ",E2.count_points(1)
+R2.<x> = GF(1009)[]
 
-print "multiply-by-7 map", phi7
-print "multiply-by-5 map", phi5
+g = x^5 + 270*x^4 + 289*x^3 + 659*x^2 +533*x + 399
+g.roots()
 
-#Q = E(6,5)
-#P = E(4,5)
-#phi = E.isogeny(Q)
-#phi_hat = phi.dual()
+phiE2 = EllipticCurveIsogeny(E2, g)  # generate an isogeny from kernel polynomial
+print phiE2
