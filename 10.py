1652517c88e89591c659ec18e8303a0235cb7860
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-09-29 21:23:06 -0400

    Update 10.py

--- 10.py
+++ 10.py
@@ -12,24 +12,25 @@ f = x^3 + x^2 + 28*x + 33
 f.roots()
 phi = EllipticCurveIsogeny(E, f)  # generate an isogeny from kernel polynomial
 phi_hat = phi.dual()
-print phi, phi.rational_maps()
+print "the isogeny:", phi, "\n the kernel poly:", phi.kernel_polynomial(), "\n The rational maps ", phi.rational_maps()
 
 P = E(3,36)
 print P, P.order(), 7*P
 
-for A in range(1,15):
-    for B in range(7,15):
+for A in range(0,36):
+    for B in range(0,18):
         try:
             E2 = EllipticCurve(GF(37), [0,0,0,A,B])
             for degree in range(4,8):
                 try:
                     #fd = compute_sequence_of_maps(E, E2, degree)
                     fd = compute_isogeny_starks(E, E2, degree)
-#                phifd = EllipticCurveIsogeny(E, fd), need fd sqrt, but....
-                    print degree, fd, fd.roots()
+                    #phi = EllipticCurveIsogeny(E, fd)
+                    print degree, A, B, E2.j_invariant(), fd.factor()
                 except (ValueError):
-                    print "Oops, no isogenies with degree", degree
-            print "end of isogenies that maps to ", E2, "j-inv:", E2.j_invariant()
+                    #print "Oops, no isogenies with degree", degree
+                    continue
+            #print "end of isogenies that maps to ", E2, "j-inv:", E2.j_invariant()
         except (ArithmeticError):
 #            print "Oops, a signular curve"
             continue
