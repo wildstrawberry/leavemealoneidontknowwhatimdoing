1375495597f44fa301119f051ae37b0925df912f
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-10-27 11:12:54 -0400

    add a nontrivial map

--- 11.py
+++ 11.py
@@ -1,19 +1,22 @@
 # from http://doc.sagemath.org/html/en/reference/curves/sage/schemes/elliptic_curves/ell_curve_isogeny.html
-from sage.schemes.elliptic_curves.ell_curve_isogeny import compute_isogeny_starks, compute_sequence_of_maps
 
 E = EllipticCurve(GF(11), [0,0,0,0,4])
-print "j-inv of E: ", E, E.j_invariant(), E.count_points(1)
+print E, "j-inv of E: ", E.j_invariant(), "#(E)=", E.count_points(1)
 E2 = EllipticCurve(GF(11), [0,0,0,5,0])
-print "j-inv of E2: ", E2, E2.j_invariant(), E2.count_points(1)
-listpoints =  E.points()
-for pos in listpoints:
-    print pos, pos.order()
+print E2, "j-inv of E2: ", E2.j_invariant(), "#(E2)=", E2.count_points(1)
 R.<x> = GF(11)[]
-f = x^2 -16*x + 64
-f.roots()
-phi = EllipticCurveIsogeny(E, f)  # generate an isogeny from kernel polynomial
+kerf = x - 8
+kerf.roots()
+phi = EllipticCurveIsogeny(E, kerf)  # generate an isogeny from kernel polynomial
 #phi_hat = phi.dual()
-#print "the isogeny:", phi, "\n the kernel poly:", phi.kernel_polynomial(), "\n The rational maps ", phi.rational_maps()
+print "the isogeny:", phi, "\n the kernel poly:", phi.kernel_polynomial(), "\n The rational maps ", phi.rational_maps()
 
-#P = E(3,36)
-#print P, P.order(), 7*P
+listpoints =  E.points()
+for PP in listpoints:
+    print PP, PP.order(), phi(PP), phi(PP).order()
+
+def Kohel_bruteforce( psi ):
+    """ generate the map (for K where char(K) is odd) directly from the kernel psi """
+    return 1/psi^2
+
+Kohel_bruteforce( kerf )
