010a9f77642f89dad39a97caf0da970890271565
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-11-24 21:13:45 -0500

    Update 11.py

--- 11.py
+++ 11.py
@@ -16,8 +16,3 @@ print "the isogeny:", phi, "\n the kernel poly:", phi.kernel_polynomial(), "\n T
 listpoints =  E.points()
 for PP in listpoints:
     print PP, PP.order(), phi(PP), phi(PP).order()
-    
-def modular3(X, Y):
-    return (X+Y)^4 - X^3*Y^3 + 2232*X^2*Y^2*(X+Y)+36864000*(X+Y)^3 - 1069960*X*Y*(X + Y)^2 + 2590058000*X^2*Y^2 + 8900112384000*X*Y*( X + Y ) + 452984832000000*(X + Y)^2 - 771751936000000000*X*Y + 1855425871872000000000*(X + Y)
-
-modular3(RRR(0),RRR(1))
