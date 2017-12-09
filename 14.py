de8fab73daabe1b07208e2c3ea6b07abc504f8d0
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-12-08 22:28:52 -0500

    volcano study

---
+++ 14.py
@@ -0,0 +1,34 @@
+# Parse the coefficients from https://math.mit.edu/~drew/ClassicalModPolys.html 
+# e.g. from 
+# [3,0] 1
+# [2,0] -162000
+# [2,1] 1488
+# [2,2] -1
+# [1,0] 8748000000
+# [1,1] 40773375
+# [0,0] -157464000000000
+# to X3 - X2Y2 + 1488X2Y - 162000X2 + 1488XY2+ 4077375XY + 8748000000X + Y3 - 162000Y2 + 8748000000Y -157464000000000. 
+
+import math
+import sys
+import time
+import numpy as np
+#from scipy import linalg
+
+NUM_ROW = 7
+
+def main():
+
+	infile = open("phi_j_2")
+	coeff = []
+	for i in xrange(0,NUM_ROW):
+		it = infile.readline()
+		print i, it
+			#for d in xrange(0,DIM):
+			#	vec.append( int(infile.readline().replace("[","").replace("]\n","")) )
+			#infile.readline()
+	print coeff		
+			
+	infile.close()
+	
+main()
