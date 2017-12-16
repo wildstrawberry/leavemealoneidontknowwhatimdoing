dac793ebc4cd11fd2f067f9e54762b9cc6ceb677
wildstrawberry <wildstrawberry@users.noreply.github.com>
2017-12-16 18:46:10 -0500

    Update 13.py

--- 13.py
+++ 13.py
@@ -1,5 +1,12 @@
-for d in [-1, -2..-300]:
+# reference: http://doc.sagemath.org/html/en/reference/number_fields/sage/rings/number_field/class_group.html
+
+for d in [-1, -2..-600]:
     if is_fundamental_discriminant(d):
         h = QuadraticField(d, 'a').class_number()
-        if is_prime(h) and h>10:
+        if is_prime(h) and h>6:
             print d, h, is_prime(h)
+
+K.<a> = NumberField(x^2+167)
+K.class_number()
+Cl = K.class_group()
+[c.representative_prime() for c in Cl]
