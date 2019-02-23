0d52f323c5ca328b25a92cd6709355b643474b3b
wildstrawberry <wildstrawberry@users.noreply.github.com>
2019-02-23 01:14:47 -0500

    Update DNN_divisibility.py

--- DNN_divisibility.py
+++ DNN_divisibility.py
@@ -1,21 +1,18 @@
 # -*- coding: utf-8 -*-
 """
 https://pytorch.org/tutorials/beginner/examples_tensor/two_layer_net_numpy.html#sphx-glr-beginner-examples-tensor-two-layer-net-numpy-py
-
-Test odd or even.
-
---------------
-
+Training to learn odd or even.
 """
 import numpy as np
 
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 4, 10, 100, 2
+N, D_in, H, D_out = 1000, 2, 10, 2
 
 # Create random input and output data
-x = np.random.randn(N, D_in)
-y = np.random.randn(N, D_out)
+x = np.random.randint(2, size=(N, D_in))
+y = [ [ xx[0], 1-xx[0] ] for xx in x ]
+#y = np.random.randint(2, size=(N, D_out))
 
 # Randomly initialize weights
 w1 = np.random.randn(D_in, H)
@@ -23,8 +20,9 @@ w2 = np.random.randn(H, D_out)
 
 learning_rate = 1e-6
 
-print x
-for t in range(50):
+#print x
+#print y
+for t in range(10000):
     # Forward pass: compute predicted y
     h = x.dot(w1)
     h_relu = np.maximum(h, 0)
@@ -32,7 +30,8 @@ for t in range(50):
 
     # Compute and print loss
     loss = np.square(y_pred - y).sum()
-    print(t, loss)
+    if (t<10) or (t%400 == 0):
+        print(t, loss)#, y_pred
 
     # Backprop to compute gradients of w1 and w2 with respect to loss
     grad_y_pred = 2.0 * (y_pred - y)
