d25633948da55621dd600ddb04ce0cf965a023fe
wildstrawberry <wildstrawberry@users.noreply.github.com>
2019-02-23 23:46:36 -0500

    Update DNN_divisibility.py

--- DNN_divisibility.py
+++ DNN_divisibility.py
@@ -7,40 +7,48 @@ import numpy as np
 
 # N is batch size; D_in is input dimension;
 # H is hidden dimension; D_out is output dimension.
-N, D_in, H, D_out = 1000, 2, 10, 2
+N, H = 500, [2, 10, 2]
+D_in, D_out = H[0], H[-1]
+print D_in, D_out
+
+ITER = 4000
+ITER_print = ITER/10
 
 # Create random input and output data
 x = np.random.randint(2, size=(N, D_in))
 y = [ [ xx[0], 1-xx[0] ] for xx in x ]
+#y = [ [ xx[0], xx[1] ] for xx in x ]
 #y = np.random.randint(2, size=(N, D_out))
 
+w = []
 # Randomly initialize weights
-w1 = np.random.randn(D_in, H)
-w2 = np.random.randn(H, D_out)
+for i in range(len(H)-1):
+    w.append( np.random.randn(H[i], H[i+1]) )
 
 learning_rate = 1e-6
 
 #print x
 #print y
-for t in range(10000):
+
+for t in range(ITER+1):
     # Forward pass: compute predicted y
-    h = x.dot(w1)
+    h = x.dot(w[0])
     h_relu = np.maximum(h, 0)
-    y_pred = h_relu.dot(w2)
+    y_pred = h_relu.dot(w[1])
 
     # Compute and print loss
     loss = np.square(y_pred - y).sum()
-    if (t<10) or (t%400 == 0):
+    if (t<10) or (t%ITER_print == 0):
         print(t, loss)#, y_pred
 
     # Backprop to compute gradients of w1 and w2 with respect to loss
     grad_y_pred = 2.0 * (y_pred - y)
-    grad_w2 = h_relu.T.dot(grad_y_pred)
-    grad_h_relu = grad_y_pred.dot(w2.T)
+    grad_w1 = h_relu.T.dot(grad_y_pred)
+    grad_h_relu = grad_y_pred.dot(w[1].T)
     grad_h = grad_h_relu.copy()
     grad_h[h < 0] = 0
-    grad_w1 = x.T.dot(grad_h)
+    grad_w0 = x.T.dot(grad_h)
 
     # Update weights
-    w1 -= learning_rate * grad_w1
-    w2 -= learning_rate * grad_w2
+    w[0] -= learning_rate * grad_w0
+    w[1] -= learning_rate * grad_w1
